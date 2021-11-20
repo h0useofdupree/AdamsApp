4e32341ac796770a641cb82469fcd011c01080b6
lucass2601 <lucas.fischer2002@t-online.de>
2021-11-20 20:16:30 +0100

    ui vorschlag

--- adams-app/src/router/index.js
+++ adams-app/src/router/index.js
@@ -1,16 +1,39 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import Home from '../views/Home.vue'
+
+import Home from '@/views/Home.vue'
+
+import Login from '@/views/Login.vue'
+
+import Dashboard from '@/views/Dashboard.vue'
+import Plan from '@/views/Plan.vue'
 
 const routes = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
   },
   {
     path: '/home',
-    name: 'Home',
-    component: Home
-  }
+    component: Home,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: Dashboard
+      },
+      {
+        path: '/plan',
+        name: 'Plan',
+        component: Plan
+      }
+    ]
+  },
+
 ]
 
 const router = createRouter({
--- adams-app/src/views/Home.vue
+++ adams-app/src/views/Home.vue
@@ -1,82 +1,82 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
+  <ion-menu side="start" content-id="main-content">
+    <ion-header>
+      <ion-toolbar translucent>
+        <ion-title>Menü</ion-title>
       </ion-toolbar>
     </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>
-          Start with Ionic
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://ionicframework.com/docs/components"
-            >UI Components</a
-          >
-        </p>
-      </div>
+    <ion-content>
+      <ion-list>
+        <ion-item href="/dashboard">
+          <ion-label>Dashboard</ion-label>
+        </ion-item>
+        <ion-item href="/plan">
+          <ion-label>Plan</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>Termine</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>Klausuren</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>Hausaufgaben</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>Noten</ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>Einstellungen</ion-label>
+        </ion-item>
+      </ion-list>
     </ion-content>
-  </ion-page>
+  </ion-menu>
+
+  <div class="ion-page" id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons>
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>{{ $route.name }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-router-outlet />
+    </ion-content>
+  </div>
 </template>
+
 
 <script>
 import {
   IonContent,
   IonHeader,
-  IonPage,
+  IonItem,
+  IonList,
+  IonMenu,
+  IonMenuButton,
+  IonButtons,
   IonTitle,
   IonToolbar,
+  IonLabel,
+  IonRouterOutlet,
 } from "@ionic/vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "Home",
   components: {
     IonContent,
     IonHeader,
-    IonPage,
+    IonItem,
+    IonList,
+    IonMenu,
+    IonMenuButton,
+    IonButtons,
     IonTitle,
     IonToolbar,
+    IonLabel,
+    IonRouterOutlet,
   },
 });
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-
-  color: #8c8c8c;
-
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
