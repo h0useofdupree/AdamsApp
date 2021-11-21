6d0158b027ead893c980fe574a6f0ad8d91b7dba
lucass2601 <lucas.fischer2002@t-online.de>
2021-11-21 16:38:51 +0100

    merged ui_example to main

--- adams-app/src/App.vue
+++ adams-app/src/App.vue
@@ -1,89 +1,18 @@
 <template>
-  <ion-header>
-    <ion-toolbar>
-      <ion-title>
-        Header
-      </ion-title>
-    </ion-toolbar>
-  </ion-header>
-  <ion-content>
-    <ion-fab vertical="top" horizontal="end">
-      <ion-fab-button>
-        <ion-icon name="add"></ion-icon>
-      </ion-fab-button>
-    </ion-fab>
-    <ion-fab vertical="center" horizontal="center">
-      <ion-fab-button>
-        <ion-icon name="settings"></ion-icon>
-      </ion-fab-button>
-      <ion-fab-list side="end">
-        <ion-fab-button><ion-icon name="logo-ionic"></ion-icon></ion-fab-button>
-        <ion-fab-button><ion-icon name="logo-vue"></ion-icon></ion-fab-button>
-      </ion-fab-list>
-    </ion-fab>
-  </ion-content>  
-
-  <ion-footer>
-    <ion-toolbar>
-      <ion-title>Footer</ion-title>
-    </ion-toolbar>
-  </ion-footer>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
 </template>
 
 <script>
-
-import { 
-  IonContent, 
-  IonFab, 
-  IonFabButton, 
-  IonFabList, 
-  IonFooter, 
-  IonHeader, 
-  IonIcon, 
-  IonTitle, 
-  IonToolbar 
-} from '@ionic/vue';
-import { 
-  add, 
-  arrowBackCircle,
-  arrowForwardCircle, 
-  arrowUpCircle,
-  logoFacebook, 
-  logoInstagram, 
-  logoTwitter, 
-  logoVimeo, 
-  person, 
-  settings, 
-  share
-} from 'ionicons/icons';
-import { defineComponent } from 'vue';
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
+  name: "App",
   components: {
-    IonContent,
-    IonFab,
-    IonFabButton,
-    IonFabList,
-    IonFooter,
-    IonHeader,
-    IonIcon,
-    IonTitle,
-    IonToolbar
+    IonApp,
+    IonRouterOutlet,
   },
-  setup() {
-    return {
-      add,
-      arrowBackCircle,
-      arrowForwardCircle,
-      arrowUpCircle,
-      logoFacebook,
-      logoInstagram,
-      logoTwitter,
-      logoVimeo,
-      person,
-      settings,
-      share
-    }
-  }
 });
 </script>
