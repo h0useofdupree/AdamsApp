ae70eb5094ea15d3906e364d42ab7fddb0d98208
lucass2601 <lucas.fischer2002@t-online.de>
2021-11-24 10:34:06 +0100

    added login page

--- adams-app/src/views/Login.vue
+++ adams-app/src/views/Login.vue
@@ -1,10 +1,48 @@
 <template>
-  <h1>Login</h1>
+  <div>
+    <form>
+      <ion-item>
+        <ion-label position="floating">Username</ion-label>
+        <ion-input
+          v-model="user.username"
+          placeholder="Gebe deinen Usernamen ein"
+        ></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label position="floating">Schlüssel</ion-label>
+        <ion-input
+          v-model="user.key"
+          placeholder="Gebe deinen Schlüssel ein"
+        ></ion-input>
+      </ion-item>
+      <ion-button @click="login" expand="block" size="medium">Login</ion-button>
+    </form>
+  </div>
 </template>
 
-<script>
-export default {};
-</script>
 
-<style>
-</style>
+<script>
+import { IonItem, IonLabel, IonInput } from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    IonItem,
+    IonLabel,
+    IonInput,
+  },
+  data() {
+    return {
+      user: {
+        username: "",
+        key: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.user);
+    },
+  },
+});
+</script>
